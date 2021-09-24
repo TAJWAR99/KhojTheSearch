9b9a656a013d21f0c2464ed6dfc7d79c21bb01ec
skatik <skatiktajwar14@gmail.com>
2021-09-24 23:43:10 +0600

    add new

--- AmiCodingPariNa/settings.py
+++ AmiCodingPariNa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f#)e-d&*e2vtdmkw7qjwa20so3co_73g4x7b2u-8(ymvv$5=+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tajwar99-khoj-the-search.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
